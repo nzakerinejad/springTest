d27bc7c9f317e5fdedaa5476f0fb140da482885c
Negar <n.zakerinejad@gmail.com>
2024-05-12 20:32:06 +0200

    adding get endpoint for mongodb

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -2,15 +2,34 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import java.util.List;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
     @GetMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping("/hassan")
+    public String hassan() {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("name").is("Eric"));
+        List<User> users = mongoTemplate.find(query, User.class);
+        if (users.size() == 0)
+            return "no user found";
+        return users.get(0).getName();
     }
 
 }
